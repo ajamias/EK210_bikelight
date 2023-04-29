18f27ea2f2ba904fe75d0bdbed112f8b189d67ec
Austin <ajamias@bu.edu>
2023-04-29 15:57:29 -0400

    initial commit

---
+++ EK210_bikelight.ino
@@ -0,0 +1,85 @@
+#include <Wire.h>
+#include <BH1750.h>
+#include <ADXL335.h>
+
+#define THRESHOLD 100
+#define DATA_SIZE 256   // change the int types to comply with data size
+#define ON 255
+#define OFF 0
+#define PIN9 9
+
+BH1750 sensor;
+ADXL335 adxl;
+float x, y, z;
+
+uint16_t data[DATA_SIZE];
+float average;
+uint16_t *current;
+
+float calc_avg(uint16_t arr[], int n);
+
+void setup()
+{
+    Serial.begin(9600);
+
+    Wire.begin();
+    adxl.begin();
+    
+    if (sensor.begin())
+        Serial.println("----- Starting -----");
+    else {
+        Serial.println("----- Error -----");
+        while(1);
+    }
+
+    adxl.getAcceleration(&x, &y, &z);
+    Serial.println(x);
+    Serial.println(y);
+    Serial.println(z);
+    
+    average = 0;
+    // initialize data array to some initial reading
+    for (unsigned int i = 0; i < DATA_SIZE; ++i) {
+        data[i] = sensor.readLightLevel();
+        average += (float) (unsigned int) data[i];
+    }
+    average /= DATA_SIZE;
+    current = data;
+}
+
+void loop()
+{  
+/*
+    do {
+        adxl.getAcceleration(&x, &y, &z);
+    } while (x < 1.65 && y < y_threshhold && z < z_threshhold);
+
+    Serial.println(x);
+    Serial.println(y);
+    Serial.println(z);
+*/
+    average -= (float) (unsigned int) *current / DATA_SIZE;
+    *current = sensor.readLightLevel();
+    average += (float) (unsigned int) *current / DATA_SIZE;
+
+    if (average < THRESHOLD) {
+        analogWrite(PIN9, ON);
+        delay(10 - average / 10);
+        analogWrite(PIN9, OFF);
+        delay(average / 10);
+    }
+
+    if (++current == data + DATA_SIZE) {
+        current = data;
+        average = calc_avg(data, DATA_SIZE);
+    }
+}
+
+float calc_avg(uint16_t arr[], int n) {
+    float avg = 0;
+    uint16_t *temp = arr;
+    do {
+        avg += *temp;
+    } while (++temp != arr + n);
+    return avg / n;
+}
